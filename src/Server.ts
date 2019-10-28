6c022ecb328f8b8349fe82cf02448e7770fe67e7
Usman Saleem <usman@usmans.info>
2019-10-28 17:59:10 +1000

    Setting up access logs using morgan

--- src/Server.ts
+++ src/Server.ts
@@ -1,8 +1,9 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import { Request, Response } from "express";
-import logger from "morgan";
+import morgan from "morgan";
 import path from "path";
+import rfs from "rotating-file-stream";
 import BaseRouter from "./routes";
 import BlogViewRouter from "./routes/BlogView";
 
@@ -10,16 +11,26 @@ import BlogViewRouter from "./routes/BlogView";
 const app = express();
 
 // Add middleware/settings/routes to express.
-app.use(logger("dev"));
+
+// set up morgan access logging
+app.use(
+  morgan("combined", {
+    stream: rfs("access.log", {
+      interval: "1d", // rotate daily
+      path: "./logs"
+    })
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use("/rest", BaseRouter);
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
+// set up routes
+app.use("/rest", BaseRouter);
 app.use("/view", BlogViewRouter);
 
 // default landing page
