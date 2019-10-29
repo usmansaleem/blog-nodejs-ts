3d4d0629dbf17a9d75b1651ea59ae03e5ed0306c
Usman Saleem <usman@usmans.info>
2019-10-29 14:59:11 +1000

    Git Ignoring env/development.env and test.env
     -- Copy .sample to .env if it doesnt exist

--- env/index.js
+++ env/index.js
@@ -1,11 +1,26 @@
-let dotenv = require('dotenv');
+let dotenv = require("dotenv");
+const fs = require("fs");
+const { COPYFILE_EXCL } = fs.constants; // cause failure if dest exists
 
 // Set default to "development"
-const nodeEnv = process.env.ENV_FILE || 'development';
+const nodeEnv = process.env.ENV_FILE || "development";
+
+// Copy from .env.sample to .env if required
+const envFile = `./env/${nodeEnv}.env`;
+const sampleEnvFile = `${envFile}.sample`;
+if (!fs.existsSync(envFile)) {
+  try {
+    fs.copyFileSync(sampleEnvFile, envFile, COPYFILE_EXCL);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// load .env file
 const result2 = dotenv.config({
-    path: `./env/${nodeEnv}.env`,
+  path: envFile
 });
 
 if (result2.error) {
-    throw result2.error;
+  throw result2.error;
 }
